23f8b3c3742445bd45e9e03376d20ecd752118c3
Naveen <knk1988@gmail.com>
2016-06-25 14:06:04 +0800

    Find Max Value K Given an array of positive integers, find maximum possible value K such that the array has at-least K elements that are greater than or equal to K. The array is unsorted and may contain duplicate values.

---
+++ FidMaxK/FidMaxK/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FidMaxK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = new int[] { 4,5,6,7,8 };
+            int val = FindMaxValueK(input);
+            Console.Write(val);
+            Console.Read();
+        }
+
+        public static int FindMaxValueK(int[] arr)
+        {
+            int n = arr.Length;
+            int[] freq = Enumerable.Repeat(0, n + 1).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] < n)
+                    freq[arr[i]]++;
+                else
+                    freq[n]++;
+            }
+
+            int sum = 0;
+
+            for (int ans = n; ans > 0; ans--)
+            {
+                sum += freq[ans];
+
+                if (sum >= ans)
+                    return ans;
+
+            }
+            return -1;
+
+        }
+    }
+}
